aa93f500953e4f5ba862ff8eb5d1bf85a264b15b
Juan Gerardo <jmendez1025@cue.edu.co>
2023-06-26 22:05:26 -0500

    feat(frontend): Base nav and home section implemented

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import { Home } from "./components/ui/Home";
+import { Nav } from "./components/ui/Nav";
+const App=()=> {
+
+  return (
+    <div >
+      <Nav/>
+      <Home/>
+
+    </div>
+  )
+}
+
+export default App
