d12235d4df791cbb3eaf36ef5a653c9c79012d12
Juan Gerardo <jmendez1025@cue.edu.co>
2023-06-27 20:49:21 -0500

    feat(frontend): Snap scroll implemented and new section base

--- frontend/src/components/ui/Nav.tsx
+++ frontend/src/components/ui/Nav.tsx
@@ -3,34 +3,38 @@ import { HiOutlineUserCircle } from "react-icons/hi";
 
 export const Nav = () => {
   return (
-    <nav className="h-28 fixed z-10 w-full ">
-      <ul className="list-none flex  w-full  text-white text-3xl font-bebas ">
-        <div className="flex-grow w-96">
-
-        </div>
-        <div className="flex items-center justify-evenly gap-x-10 w-[50%]">
-          <li  className="w-32">
-            <a className=" hover:border-b-4 border-transparent hover:border-y-yellow-300 cursor-pointer">INICIO</a>
+    <nav className="h-28 fixed z-30 w-full ">
+      <ul className="list-none flex  w-full  text-white text-3xl font-bebas  ">
+        <div className="flex-grow w-96"></div>
+        <div className="flex items-center justify-evenly gap-x-10 w-[50%] [&>li>a]:transition-all">
+          <li className="w-32">
+            <a className=" hover:border-b-4 border-transparent hover:border-y-yellow-300 cursor-pointer">
+              INICIO
+            </a>
           </li>
-          <li  className="w-32">
-            <a className=" hover:border-b-4 border-transparent hover:border-y-yellow-300 cursor-pointer">Sedes</a>
+          <li className="w-32">
+            <a className=" hover:border-b-4 border-transparent hover:border-y-yellow-300 cursor-pointer">
+              Sedes
+            </a>
           </li>
 
-
-          <li >
+          <li>
             <img src={logo}></img>
           </li>
 
-          <li  className="w-32">
-            <a className=" hover:border-b-4 border-transparent hover:border-y-yellow-300 cursor-pointer">Reservas</a>
+          <li className="w-32">
+            <a className=" hover:border-b-4 border-transparent hover:border-y-yellow-300 cursor-pointer">
+              Reservas
+            </a>
           </li>
-          <li  className="w-32">
-            <a className=" hover:border-b-4 border-transparent hover:border-y-yellow-300 cursor-pointer">Barberos</a>
+          <li className="w-32">
+            <a className=" hover:border-b-4 border-transparent hover:border-y-yellow-300 cursor-pointer">
+              Barberos
+            </a>
           </li>
-    
         </div>
 
-        <div className=" flex items-center justify-end w-96 flex-grow" >
+        <div className=" flex items-center justify-end w-96 flex-grow">
           <HiOutlineUserCircle className="text-4xl mx-4" />
         </div>
       </ul>
