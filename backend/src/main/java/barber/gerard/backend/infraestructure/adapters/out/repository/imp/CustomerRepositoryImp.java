df2b7ef1485e24085ceba443b81bf34423d4316c
Juan Gerardo <jmendez1025@cue.edu.co>
2023-10-16 11:34:34 -0500

    refactor(Customer Repository): CRUD refactored using EntityManager

--- backend/src/main/java/barber/gerard/backend/infraestructure/adapters/out/repository/imp/CustomerRepositoryImp.java
+++ backend/src/main/java/barber/gerard/backend/infraestructure/adapters/out/repository/imp/CustomerRepositoryImp.java
@@ -1,51 +1,71 @@
 package barber.gerard.backend.infraestructure.adapters.out.repository.imp;
 
+import barber.gerard.backend.domain.enums.Role;
 import barber.gerard.backend.domain.models.Customer;
+import barber.gerard.backend.domain.models.User;
 import barber.gerard.backend.infraestructure.adapters.out.repository.JpaCustomerRepository;
 import barber.gerard.backend.infraestructure.entities.CustomerEntity;
 import barber.gerard.backend.infraestructure.mapping.customer.CustomerMapper;
 import barber.gerard.backend.infraestructure.ports.out.CustomerRepository;
 import barber.gerard.backend.infraestructure.ports.out.UserRepository;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
-@Component
+@Repository
+@Transactional
 @AllArgsConstructor
 public class CustomerRepositoryImp implements CustomerRepository {
     private JpaCustomerRepository jpaCustomerRepository;
     private CustomerMapper customerMapper;
     private UserRepository userRepository;
+    @PersistenceContext
+    private EntityManager entityManager;
 
 
     @Override
     public Customer save(Customer customer) {
-        CustomerEntity customerEntity = customerMapper.domainToEntity(customer);
-        CustomerEntity entitySaved =  jpaCustomerRepository.save(customerEntity);
-        return customerMapper.entityToDomain(entitySaved);
+        Customer customerSaved = customerMapper.userToCustomer(userRepository.save(customer));
+        customerSaved.setPoints(customer.getPoints());
+        saveCustomerPoints(customerSaved);
+
+        return customerSaved;
     }
 
     @Override
     public Optional<Customer> findById(Long id) {
-        Optional<CustomerEntity> customerEntity = jpaCustomerRepository.findById(id);
-        return customerEntity.map(cust-> customerMapper.entityToDomain(cust));
+        Optional<User> user = userRepository.findById(id);
+        if(user.isPresent()){
+            Customer customer = customerMapper.userToCustomer(user.get());
+            customer.setPoints(getCustomerPoints(customer));
+
+            return Optional.of(customer);
+        }else {
+            return Optional.empty();
+        }
     }
 
     @Override
     public List<Customer> findAll() {
-        return customerMapper.entityListToDomainList(jpaCustomerRepository.findAll());
+        List<Customer> customers = customerMapper.usetListToCustomerList(userRepository.findByRole(Role.CUSTOMER));
+        customers.parallelStream()
+                    .forEach(c->c.setPoints(getCustomerPoints(c)));
+
+        return customers;
     }
 
     @Override
     public Optional<Customer> update(Customer customerUpdated) {
-        if(jpaCustomerRepository.existsById(customerUpdated.getId())){
-            CustomerEntity customerEntity = customerMapper.domainToEntity(customerUpdated);
-            CustomerEntity entityUpdated =  jpaCustomerRepository.save(customerEntity);
-            return Optional.of(
-                    customerMapper.entityToDomain(entityUpdated)
-            );
+        if(userRepository.existsById(customerUpdated.getId())){
+            Customer customer = customerMapper.userToCustomer(userRepository.update(customerUpdated).get());
+            updateCustomerPoints(customerUpdated);
+            customer.setPoints(getCustomerPoints(customer));
+            return Optional.of(customer);
         }else {
             return Optional.empty();
         }
@@ -53,8 +73,33 @@ public class CustomerRepositoryImp implements CustomerRepository {
 
     @Override
     public Customer deleteById(Long id) {
-        Optional<CustomerEntity> customer =  jpaCustomerRepository.findById(id);
-        return customer.map(cust -> customerMapper.entityToDomain(cust))
-                .orElseThrow(()-> new RuntimeException());
+        deleteCustomerPoints(id);
+        userRepository.deleteById(id);
+        //TODO: Return Customer.
+        return null;
+
+    }
+
+    private void saveCustomerPoints(Customer customer){
+        entityManager.createNativeQuery("INSERT INTO customer_points(customer_id,points) VALUES(?,?)")
+                                        .setParameter(1,customer.getId())
+                                        .setParameter(2,customer.getPoints())
+                                        .executeUpdate();
+    }
+
+    private Integer getCustomerPoints(Customer customer){
+        return (Integer) entityManager.createNativeQuery("SELECT points FROM customer_points WHERE customer_id = ?")
+                                                        .setParameter(1,customer.getId())
+                                                        .getSingleResult();
+    }
+    private void deleteCustomerPoints(Long customerId){
+        entityManager.createNativeQuery("DELETE FROM customer_points WHERE customer_id = ?")
+                                        .setParameter(1,customerId)
+                                        .executeUpdate();
+    }
+
+    private void updateCustomerPoints(Customer customer){
+        deleteCustomerPoints(customer.getId());
+        saveCustomerPoints(customer);
     }
 }
