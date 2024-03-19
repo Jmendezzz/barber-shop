85a211a93d86b0d1e47ade5883522f50f6c9fc3d
Juan Gerardo <jmendez1025@cue.edu.co>
2024-03-18 21:47:58 -0500

    feat: register route and base implemented

--- frontend/src/ui/login/LoginFooter.tsx
+++ frontend/src/ui/login/LoginFooter.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import GoogleSvg from "../GoogleSvg";
 import Row from "../Row";
 
@@ -6,9 +7,11 @@ function LoginFooter() {
     <Row center={true}>
         <Row type="horizontal" center={true} gap={1}>
         <span className="text-white text-sm">¿No tienes cuenta?</span>
-        <a href="#" className="underline text-sm text-white">
-            Registrate
-        </a>
+        <Link to="/register">
+            <p className="underline text-sm text-white">
+                Registrate
+            </p>
+        </Link>
         </Row>
         <p className="font-bebasNeue text-3xl">ó continua con:</p>
         <div className="border rounded-full p-2 bg-gray-900 cursor-pointer">
