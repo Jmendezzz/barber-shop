bd18a61dec36563063a808db9c7dc884bbe688bd
Juan Gerardo <jmendez1025@cue.edu.co>
2024-03-23 14:50:21 -0500

    feat: register form implemented

--- frontend/src/ui/Form.tsx
+++ frontend/src/ui/Form.tsx
@@ -3,11 +3,14 @@ import styled from 'styled-components';
 const Form = styled.form`
   overflow: hidden;
   font-size: 1.4rem;
-  padding: 2rem 0.2rem;
+  padding: 1rem 0.2rem;
   display:flex;
   flex-direction: column;
   width: 100%;
   gap: 1.2rem;
+  & button {
+    margin-top: 1.2rem;
+  }
 `;
 
 export default Form;
